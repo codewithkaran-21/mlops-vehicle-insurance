e017b710457df6d84431a88f169faf0beb34633e
Karan <gotthatback21@gmail.com>
2025-04-14 11:36:43 +0530

    model training done

---
+++ src/entity/estimator.py
@@ -0,0 +1,27 @@
+import sys
+
+import pandas as pd
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+
+from src.exception import MyException
+from src.logger import logging
+
+class TargetValueMapping:
+    def __init__(self):
+        self.yes:int = 0
+        self.no:int = 1
+    def _asdict(self):
+        return self.__dict__
+    def reverse_mapping(self):
+        mapping_response = self._asdict()
+        return dict(zip(mapping_response.values(),mapping_response.keys()))
+
+class MyModel:
+    def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
+        """
+        :param preprocessing_object: Input Object of preprocesser
+        :param trained_model_object: Input Object of trained model 
+        """
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
